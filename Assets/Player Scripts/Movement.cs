cf17ee96139f5edfbaa5b90f25a0b0d2d166d76c
hydroguy45 <foleychriscpfjr@gmail.com>
2017-09-23 18:12:44 -0400

    Gravity is a thing now

---
+++ Assets/Player Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starting Ground Sensor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Platforms") {
+			Debug.Log ("Touched Platform");
+		}
+	}
+
+}
