be8bbe08703cb6c8405a84c29e361480e79d0704
suyogbob <suyogbobhate@gmail.com>
2017-10-28 16:11:53 -0400

    Loop Infinitely
    You can now loop infinitely between our two levels

--- Assets/Player Scripts/NextLevel.cs
+++ Assets/Player Scripts/NextLevel.cs
@@ -9,10 +9,13 @@ public class NextLevel : MonoBehaviour {
 	public Rigidbody2D player;
 	public string levelToLoad;
 	private Collider2D diamondCollider;
+	private Light spot;
 
 	// Use this for initialization
 	void Start () {
 		diamondCollider = player.GetComponent<Collider2D> ();
+		spot = GameObject.Find("Spotlight").GetComponent<Light> ();
+		spot.transform.localPosition = new Vector2 (0f, 1.5f);
 	}
 	
 	// Update is called once per frame
