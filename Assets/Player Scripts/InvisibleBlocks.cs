549361bc9906e2faf81a864de09662ab20f1a425
suyogbob <suyogbobhate@gmail.com>
2017-10-28 14:29:47 -0400

    Distance from platform toggles visibility
    Alex's changes to invisble plotforms toggling based on distance from player

--- Assets/Player Scripts/InvisibleBlocks.cs
+++ Assets/Player Scripts/InvisibleBlocks.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class InvisibleBlocks : MonoBehaviour {
-
 	public GameObject[] invis;
+	public GameObject player;
 	public Light playerLight;
 	private float timeSinceUse;
 	private SpriteRenderer[] ren;
@@ -13,6 +12,7 @@ public class InvisibleBlocks : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		playerLight = GameObject.Find("Point light").GetComponent<Light> ();
+		player = GameObject.Find ("Player");
 		timeSinceUse = 10f;
 		invis = GameObject.FindGameObjectsWithTag ("Invisible");
 		ren = new SpriteRenderer[invis.Length];
@@ -20,30 +20,25 @@ public class InvisibleBlocks : MonoBehaviour {
 			ren [i] = invis [i].GetComponent<SpriteRenderer> ();
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		for (int i = 0; i < invis.Length; i++) {
+			if(timeSinceUse < 1f && playerLight.intensity == 5f && Vector3.Distance(invis[i].transform.position, player.transform.position) < 5)
+				ren[i].sprite = rusty;
+			else
+				ren[i].sprite = none;
+		}
 		if (timeSinceUse > 1f && playerLight.intensity == 5f) {
 			playerLight.intensity = 0.4f;
-			for (int i = 0; i < invis.Length; i++) {
-				ren [i].sprite = none;
-			}
 		}
 	}
-
 	void FixedUpdate () {
 		if (Input.GetKey (KeyCode.F)) {
-			LightFlash ();
+			playerLight.intensity = 5f;
+			timeSinceUse = 0f;
 		}
 		timeSinceUse += Time.deltaTime;
 	}
 
-	void LightFlash () {
-		playerLight.intensity = 5f;
-		for (int i = 0; i < invis.Length; i++) {
-			ren [i].sprite = rusty;
-		}
-		timeSinceUse = 0f;
-	}
-			
 }
