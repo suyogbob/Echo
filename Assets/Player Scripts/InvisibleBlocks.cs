4a0addd3bcdc5e84a8c1b05e35710d9d2e35e9cc
Peffern <agoodisman@nichols-ve502-0974.apn.wlan.upenn.edu>
2017-11-11 14:14:57 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 232e682f55b765863e82515b48c4845d0318fd78, reversing
    changes made to 86abaa092bc63b4ce446deaebd4889cfb9cf8e25.

--- Assets/Player Scripts/InvisibleBlocks.cs
+++ Assets/Player Scripts/InvisibleBlocks.cs
@@ -1,44 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class InvisibleBlocks : MonoBehaviour {
-    public GameObject[] invis;
-    public GameObject player;
-    public Light playerLight;
-    private float timeSinceUse;
-    private SpriteRenderer[] ren;
-    public Sprite rusty;
-    public Sprite none;
-    // Use this for initialization
-    void Start () {
-        playerLight = GameObject.Find("Point light").GetComponent<Light> ();
-        player = GameObject.Find ("Player");
-        timeSinceUse = 10f;
-        invis = GameObject.FindGameObjectsWithTag ("Invisible");
-        ren = new SpriteRenderer[invis.Length];
-        for (int i = 0; i < invis.Length; i++) {
-            ren [i] = invis [i].GetComponent<SpriteRenderer> ();
-        }
-    }
-    
-    // Update is called once per frame
-    void Update () {
-        for (int i = 0; i < invis.Length; i++) {
-            if(timeSinceUse < 1f && playerLight.intensity == 5f && Vector3.Distance(invis[i].transform.position, player.transform.position) < 5)
-                ren[i].sprite = rusty;
-            else
-                ren[i].sprite = none;
-        }
-        if (timeSinceUse > 1f && playerLight.intensity == 5f) {
-            playerLight.intensity = 0.4f;
-        }
-    }
-    void FixedUpdate () {
-        if (Input.GetKey (KeyCode.F)) {
-            playerLight.intensity = 5f;
-            timeSinceUse = 0f;
-        }
-        timeSinceUse += Time.deltaTime;
-    }
-            
+
+	public GameObject[] invis;
+	public GameObject player;
+	public Light playerLight;
+	private float timeSinceUse;
+	private SpriteRenderer[] ren;
+	public Sprite rusty;
+	public Sprite none;
+	// Use this for initialization
+	void Start () {
+		playerLight = GameObject.Find("Point light").GetComponent<Light> ();
+		player = GameObject.Find ("Player");
+		timeSinceUse = 10f;
+		invis = GameObject.FindGameObjectsWithTag ("Invisible");
+		ren = new SpriteRenderer[invis.Length];
+		for (int i = 0; i < invis.Length; i++) {
+			ren [i] = invis [i].GetComponent<SpriteRenderer> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < invis.Length; i++) {
+			if(timeSinceUse < 1f && playerLight.intensity == 5f && Vector3.Distance(invis[i].transform.position, player.transform.position) < 5)
+				ren[i].sprite = rusty;
+			else
+				ren[i].sprite = none;
+		}
+
+		if (timeSinceUse > 1f && playerLight.intensity == 5f) {
+			playerLight.intensity = 0.4f;
+		}
+	}
+
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.F)) {
+			LightFlash ();
+		}
+		timeSinceUse += Time.deltaTime;
+	}
+
+	void LightFlash () {
+		playerLight.intensity = 5f;
+
+		timeSinceUse = 0f;
+	}
+			
 }
