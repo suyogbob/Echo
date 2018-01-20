4b921339def902b9944797784658cef9724faaea
suyogbob <suyogbobhate@gmail.com>
2018-01-20 17:47:33 -0500

    Horizontal sticking fixed
    You can't horizontally stick to platforms anymore. Standing on the edge of platforms still work if part of the player object is inside the platform's bounds.

--- Assets/Player Scripts/Movement.cs
+++ Assets/Player Scripts/Movement.cs
@@ -3,21 +3,23 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 	private Rigidbody2D rb2d;
+	private CircleCollider2D c2d;
 	private Collider2D groundSensor;
 	public float speedInitial;
 	public float jumpStrength;
-	private bool isGrounded;
+	public bool isGrounded;
 	private float speed;
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		c2d = GetComponent<CircleCollider2D> ();
 		groundSensor = GameObject.Find("Ground_Sensor").GetComponent<Collider2D>();
 		speed = speedInitial;
+		isGrounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		isGrounded = (groundSensor.IsTouchingLayers () ? true : false);
 		if (Input.GetKey (KeyCode.LeftShift)) {
 			speed = 2 * speedInitial;
 		} else {
@@ -35,8 +37,33 @@ public class Movement : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
 			Vector2 move = new Vector2 (0, 1)*jumpStrength;
 			rb2d.AddForce (move);
+			isGrounded = false;
 		}
 
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		/*if (col.bounds.center.x + col.bounds.extents.x > rb2d.position.x &&
+		   col.bounds.center.x - col.bounds.extents.x < rb2d.position.x) {
+			isGrounded = true;
+		}*/
+	}
+	void OnTriggerStay2D(Collider2D col) {
+        Rigidbody2D box = col.attachedRigidbody;
+		Debug.Log ("platform right edge: " + (col.bounds.center.x + col.bounds.extents.x));
+		Debug.Log ("player left edge: " + (c2d.bounds.center.x - c2d.bounds.extents.x));
+		Debug.Log ("\n");
+        if (col.bounds.center.x + col.bounds.extents.x > c2d.bounds.center.x - c2d.bounds.extents.x &&
+            col.bounds.center.x - col.bounds.extents.x < c2d.bounds.center.x + c2d.bounds.extents.x) {
+			isGrounded = true;
+        } else {
+            Vector2 move = new Vector2(0, -1) * speed;
+            rb2d.velocity = (move);
+            isGrounded = false;
+        }
+	}
+	void OnTriggerExit2D(Collider2D col) {
+		isGrounded = false;
 	}
 
 
