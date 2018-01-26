6c5689d18a9b7a09d2e0d92da3b1b46f74c5a1e5
Peffern <agoodisman@huntsman-ve501-0871.apn.wlan.upenn.edu>
2018-01-26 16:37:25 -0500

    firing balls while there are already balls clears the old ones

--- Assets/Player Scripts/Powers.cs
+++ Assets/Player Scripts/Powers.cs
@@ -6,12 +6,20 @@ public class Powers : MonoBehaviour {
     public Object ball;
     public int numberOfBalls;
     public float velocityScalar;
+	public GameObject firstBall;
     void Update () {
         if (Input.GetKeyDown(KeyCode.O))
         {
+			while (firstBall != null) 
+			{
+				Debug.Log ("test");
+				GameObject nextBall = firstBall.GetComponent<EchoBallConstructor> ().otherNeighbor;
+				Destroy (firstBall);
+				firstBall = nextBall;
+			}
+
             Transform parent = GetComponent<Transform>();
             Quaternion none = new Quaternion();
-            GameObject firstBall = null;
             GameObject previousBall = null;
             for (float rad = 0; rad < 2 * Mathf.PI; rad += 2 * Mathf.PI / numberOfBalls)
             {
