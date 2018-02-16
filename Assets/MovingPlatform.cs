20ad2da72071ee1360f94e57fbfbcf0d683646c5
Peffern <agoodisman@nichols-ve501-0436.apn.wlan.upenn.edu>
2018-02-16 00:55:39 -0500

    moving platform prefab and script

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private Transform transform;
+	public Vector3[] positions;
+	public float velocityScalar;
+	private int currentTarget;
+	private Vector3 start;
+
+	private float epsilon = 0.1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		transform = GetComponent<Transform> ();
+		currentTarget = 0;
+		start = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((positions [currentTarget] + start - transform.position).magnitude <= epsilon)
+			currentTarget = (currentTarget + 1) % positions.Length;
+		rb2d.velocity = (positions [currentTarget] + start - transform.position).normalized * velocityScalar;
+
+	}
+}
