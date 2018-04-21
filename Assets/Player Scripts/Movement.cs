494f081b32ba344f4e5e73cdc7526dcf8760f572
Peffern <salamander496@gmail.com>
2018-04-21 09:33:25 -0400

    smooth level transition
    test1->test2 is now smooth

--- Assets/Player Scripts/Movement.cs
+++ Assets/Player Scripts/Movement.cs
@@ -72,7 +72,7 @@ public class Movement : MonoBehaviour, IPower
 			rb2d.AddForce(move);
 		}
 		//on space press, do jump. only if standing on a platform
-		if (Input.GetKeyDown(KeyCode.W) && (platform != null))
+		if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && (platform != null))
 		{
 			//jump
 			Vector2 move = new Vector2(0, 1) * jumpStrength;
